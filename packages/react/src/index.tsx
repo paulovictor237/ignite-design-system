1d30cfcee34d02ebb61ec7da5019aa00e951174f
Paulo Victor <paulovictor237@gmail.com>
2022-11-30 22:34:49 -0300

    stitches

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,5 +1,11 @@
-import { colors } from '@ignite-ui/tokens'
+import { styled } from './styles'
+
+const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$ignite500',
+  borderRadius: '$md',
+})
 
 export const App = () => {
-  return <h1 style={{ color: colors.ignite300 }}>hello World</h1>
+  return <Button>hello World</Button>
 }
